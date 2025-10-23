1f7824685411e73d8a8a50c24a31556898571cce
Caramel-Caramel <2417006617@qq.com>
2025-10-23 14:31:42 +0800

    1

---
+++ anthony/pdf2word-with-jyutping/src/main/java/caramel/impl/pdf/PdfAnalysisPronounce.java
@@ -0,0 +1,27 @@
+package caramel.impl.pdf;
+
+import caramel.Entry;
+import caramel.impl.pronounce.GetPronounce;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PdfAnalysisPronounce {
+
+    public static void process(String pdfFileName, String txtFileName) throws IOException {
+        String[] textArray = TextGet.getText(pdfFileName);
+        StringBuilder sb = new StringBuilder();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(Entry.BASE_PATH + "\\txt\\" + txtFileName))) {
+            for (String text : textArray) {
+                sb.append(text).append("\r\n");
+                if (sb.length() > 1000) {
+                    String result = GetPronounce.getTextWithPronounce(sb.toString());
+                    writer.write(result);
+                    writer.newLine();
+                    sb = new StringBuilder();
+                }
+            }
+        }
+    }
+}
